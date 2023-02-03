6e6fcd09ce78ce06b3c986e31d1d59bb041b1b92
britofelipe <arrudabritofelipe@gmail.com>
2023-02-03 17:35:07 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// Based on Matheus Battisti Course (udemy)
+
+const express = require("express")
+const exphbs = require("express-handlebars")
+const session = require("express-session")
+const FileStore = require("session-file-store")(session)
+
+const app = express()
